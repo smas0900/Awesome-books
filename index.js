3a05d9ad99e00c3473e4a507b517acb5925d8812
smas0900 <47197167+smas0900@users.noreply.github.com>
2022-09-23 04:24:37 +0500

    Add js

---
+++ index.js
@@ -0,0 +1,51 @@
+export default class Books {
+  constructor(){
+      this.BookList = {};
+      this.StoreBooks = [];
+      this.DisplayBooks = document.querySelector('.collection');
+      this.NewTitle = document.querySelector('#title');
+      this.NewAuthor = document.querySelector('#author');
+      this.SaveBooks = localStorage.getItem('BookList');
+      this.AddBook = document.querySelector('#btn');
+      this.ErrorChecker = document.querySelector('.checker');
+      this.DisplayColor = document.querySelector('.Display');
+  }
+  
+  SaveItem(){
+  this.AddBook.addEventListener('click',()=>{
+      this.BookList={
+          book:this.NewTitle.value,
+          author:this.NewAuthor.value
+         } 
+         this.StoreBooks.push(this.BookList);
+         localStorage.setItem('Todo',JSON.stringify(this.StoreBooks))
+         this.DisplayData();
+  })
+  
+  }
+  
+  
+  DisplayData(){
+  this.DisplayBooks.innerHTML=''
+  const HoldData=JSON.parse(localStorage.getItem('Todo'))
+  if(HoldData !==null){
+      this.StoreBooks=HoldData;
+      HoldData.forEach((element,index)=>{
+          this.DisplayBooks.innerHTML+=`
+          <tr class="Display">
+          <td><span>"</span>${element.book}<span>" by </span>${element.author}</td>
+          <td><button class="remove" id="${index}">Remove</button></td>
+          </tr>`;
+      })
+  
+  }else{
+      []
+  }
+  }
+  
+  }
+  
+  const Book=new Books();
+  Book.SaveItem();
+  Book.DisplayData();
+  
