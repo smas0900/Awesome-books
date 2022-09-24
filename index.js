a787d586fa6ffd3409e8b154deb78e0526cbd85c
smas0900 <47197167+smas0900@users.noreply.github.com>
2022-09-24 11:15:04 +0500

    Add js for singlepage display

--- index.js
+++ index.js
@@ -63,3 +63,38 @@ const Book = new Books();
 Book.SaveItem();
 Book.DisplayData();
 localStorage.removeItem('bookarr');
+
+const disbook = document.querySelector('.List-items');
+const addboo = document.querySelector('.add-books');
+const Contact = document.querySelector('.Contact');
+const list = document.querySelector('#List');
+const Add = document.querySelector('#Add');
+const link = document.querySelector('#Link');
+
+export const DisplayAllBooks = () => {
+  list.addEventListener('click', () => {
+    disbook.classlist.toggle('active');
+    Contact.classList.remove('active');
+    addboo.classList.remove('active');
+    disbook.style.display = 'block';
+
+  });
+};
+
+export const AddNewBook = () => {
+  Add.addEventListener('click', () => {
+    addboo.classList.toggle('active');
+    Contact.classList.remove('active');
+    disbook.classList.remove('active');
+    disbook.style.display = 'none';
+  });
+};
+
+export const ShowContactDetails = () => {
+  link.addEventListener('click', () => {
+    Contact.classList.toggle('active');
+    addboo.classList.remove('active');
+    disbook.classList.remove('active');
+    disbook.style.display = 'none';
+  });
+};
