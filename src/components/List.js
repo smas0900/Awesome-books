bc5904ebf33212863045c956b83a1bd4e4e770c0
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-24 11:14:41 +0430

    Add List class properties

---
+++ src/components/List.js
@@ -0,0 +1,25 @@
+export default class List {
+  constructor() {
+    this._content = `
+        <section>
+            <h1 class="h1 invisible">Awesome Books</h1>
+            <div class="books"></div>
+            <table>
+                <thead>
+                  <tr>
+                    <th>Book</th>
+                    <th>Author</th>
+                    <th>remove</th>
+                  </tr>
+                </thead>
+                <tbody class="library">
+                </tbody>
+            </table>
+        </section>
+        `;
+  }
+
+  get content() {
+    return this._content;
+  }
+}
