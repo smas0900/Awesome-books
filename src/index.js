c1e790cf336afc46e54ecb823f31955de08c8355
smas0900 <47197167+smas0900@users.noreply.github.com>
2023-08-24 20:44:33 +0500

    Revert "fix linters"
    This reverts commit 6485480522247b8a6e29c7a9c96effc640320bcd.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { DateTime } from './components/luxun.js';
 import Form from './components/Form.js';
 import List from './components/List.js';
 import Contact from './components/Contact.js';
@@ -44,3 +45,14 @@ navLinks.forEach((element) => {
     }
   });
 });
+
+const updateTimeDisplay = () => {
+  const timeDisplay = document.getElementById('timeDisplay');
+  const currentDate = DateTime.now(); // eslint-disable-line no-undef
+  // Formatting the date as a string
+  const formattedDate = currentDate.toLocaleString(DateTime.DATETIME_FULL);
+  timeDisplay.textContent = formattedDate;
+};
+// updateTimeDisplay();
+updateTimeDisplay();
+setInterval(updateTimeDisplay, 1000);
